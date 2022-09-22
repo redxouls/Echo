86fdecdd1e30273fcf4443e89c73c0c712c95717
Daniel <yaotinghuang89@gmail.com>
2022-09-22 17:00:23 +0800

    add basic_echo_effect

--- Assets/player_movement.cs
+++ Assets/player_movement.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class player_movement : MonoBehaviour
 {   
     public float step_size = 1.0f;
-    public float rotation = 0.2f;
-    private float y = 0.0f;
-    private float z = 0f;
+    public GameObject prefab;
+    public GameObject head;
+    private float echo_lifeSpan = 5.0f;
+    private float timer = 0.2f;
+    private float min_echo_interval = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,32 +17,47 @@ public class player_movement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if (timer < min_echo_interval) {
+            timer += Time.deltaTime;
+        }
         if (Input.GetKey("w")) {
+            head.transform.position += new Vector3(-step_size, 0, 0) * Time.deltaTime;
             this.transform.localPosition += new Vector3(-step_size, 0, 0) * Time.deltaTime;
+            if (timer >= min_echo_interval) {
+                Echo();
+                timer = 0.0f;
+            }
         }
         if (Input.GetKey("a")) {
+            head.transform.position += new Vector3(0, 0, -step_size) * Time.deltaTime;
             this.transform.localPosition += new Vector3(0, 0, -step_size) * Time.deltaTime;
+            if (timer >= min_echo_interval) {
+                Echo();
+                timer = 0.0f;
+            }
         }
         if (Input.GetKey("s")) {
+            head.transform.position += new Vector3(step_size, 0, 0) * Time.deltaTime;
             this.transform.localPosition += new Vector3(step_size, 0, 0) * Time.deltaTime;
+            if (timer >= min_echo_interval) {
+                Echo();
+                timer = 0.0f;
+            }
         }
         if (Input.GetKey("d")) {
+            head.transform.position += new Vector3(0, 0, step_size) * Time.deltaTime;
             this.transform.localPosition += new Vector3(0, 0, step_size) * Time.deltaTime;
+            if (timer >= min_echo_interval) {
+                Echo();
+                timer = 0.0f;
+            }
         }
-        if (Input.GetKey("left")) {
-            y -= rotation;
-        }
-        if (Input.GetKey("right")) {
-            y += rotation;
-        }
-        if (Input.GetKey("up")) {
-            z -= rotation;
-        }
-        if (Input.GetKey("down")) {
-            z += rotation;
-        }
-        this.transform.localRotation = Quaternion.Euler(0, y, z);
-        // Debug.Log(this.transform.localPosition);
+    }
+
+    void Echo() {
+        GameObject echo = Instantiate(prefab, gameObject.transform.position, Quaternion.identity);
+        echo.SetActive(true);
+        Destroy(echo, echo_lifeSpan);
     }
 }
