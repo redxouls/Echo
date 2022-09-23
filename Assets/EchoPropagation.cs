ef0b8d446786deddb0b363c169fe7a980f46612d
redxouls <b08901058@ntu.edu.tw>
2022-09-23 21:47:15 +0800

    Rename variable names, script and object name to Pascal case. Add first person view control and simplify movement logic

--- Assets/EchoPropagation.cs
+++ Assets/EchoPropagation.cs
@@ -2,26 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class echo_propagation : MonoBehaviour
+public class EchoPropagation : MonoBehaviour
 {   
     private Vector3 scale = new Vector3(1, 1, 1);
     private float timer = 0.0f;
-    public float speed = 10.0f;
+    public float speed = 20.0f;
     public Transform player;
 
     // Start is called before the first frame update
     void Start()
     {   
-        this.transform.localPosition = player.transform.localPosition;
-        this.gameObject.transform.localScale = scale;
+        transform.localPosition = player.transform.localPosition;
+        transform.localScale = scale;
     }
 
     // Update is called once per frame
     void Update()
     {   
         timer += Time.deltaTime * speed;
-        this.gameObject.transform.localScale = scale * (1 + timer);
-        // Debug.Log(1 + timer);
-        // if (1 + timer >= 20) {timer = 0;}
+        transform.localScale = scale * (1 + timer);
     }
 }
---
+++ Assets/MouseLook.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 200f;
+    public float lookXLimit = 45.0f;
+
+    public Transform player;
+
+    float xRotation = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get user input and adjust with mouseSensitivity
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Calculate rotation for x-axis
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -lookXLimit, lookXLimit);
+
+        // Set calculate values to player and camera
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.transform.Rotate(Vector3.up * mouseX);
+    }
+}
