bfe0a441313fa0f648ec905381ce75548bef95a7
Daniel891116 <55268160+Daniel891116@users.noreply.github.com>
2022-11-09 01:21:33 +0800

    ECHO-#20 Add evil AI
    * Add AI
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix NavMesh problems
    
    * none
    
    * none

--- Assets/Scripts/EvilTracing.cs
+++ Assets/Scripts/EvilTracing.cs
@@ -1,71 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EvilTracing : MonoBehaviour
 {
     public SoundWaveManager soundWaveManager;
-    public float speed;
-    public CharacterController controller;
-    public Vector3 target; // set to public for the convenience of debug
-    public bool moving;
-    
     public ParticleSystem particleSystem;
-
+    public NavMeshAgent agent;
+    public float trigger_dst = 5.0f;
+    private NavMeshHit target;
+    private NavMeshPath path;
+    private bool moving;
+    private float route_length;
     // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("Trace", 1f, 0.1f);
-        target = transform.position;
+    void Start() {
+        // InvokeRepeating("Trace", 1f, 0.1f);
+        agent = GetComponent<NavMeshAgent>();
+        path = new NavMeshPath();
+        route_length = 0.0f;
+        moving = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Move();
+    void Update() {
         VisualizeEvil();
+        Trace();
     }
     void Trace() {
-        int start = soundWaveManager.startIndex;
-        int end = soundWaveManager.endIndex;
-        int len = soundWaveManager.maxNumOfWave;
-        float targetDistance = float.MaxValue;
-        for (int i = 0; i < (end + len - start) % len; ++i)
-        {
-            int index = (start + i) % len;
-            // ignore the sound wave created by environment
-            if (soundWaveManager.waveAttributes[index] == SoundWaveManager.WAVE_ATTRIBUTE.ENV)
-            {
-                continue;
-            }
-            float dist = Vector3.Distance(soundWaveManager.points[index], transform.position); // Unity would auto cast Vector4 to Vector3 by use the first three values
-            if (Mathf.Abs(dist - soundWaveManager.points[index].w) < 1)
-            {
-                // select the closest target
-                if (dist < targetDistance)
-                {
-                    targetDistance = dist;
-                    target = soundWaveManager.points[index]; // Unity would auto cast Vector4 to Vector3 by use the first three values
+        if (soundWaveManager.endIndex != soundWaveManager.startIndex) {
+            // code vector4 points to vector3 position
+            NavMesh.SamplePosition(soundWaveManager.points[soundWaveManager.endIndex - 1], out target, 5.0f, NavMesh.AllAreas);
+            Debug.Log(moving);
+            // agent.SetDestination(position);
+            if(NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path)) {
+                route_length = CalcPathDistance(path);
+                Debug.Log(route_length);          
+                if (route_length <= trigger_dst) {
+                    moving = true;
+                    agent.SetPath(path);
+                }
+                else {
+                    moving = false;
                 }
             }
-        }
-    }
-    void Move() {
-        Vector3 move = Vector3.Normalize(target - transform.position) * speed * Time.deltaTime;
-        move.y = 0;
-        transform.position = transform.position + move;
-        //controller.Move(move);
-        
-        // Use the distance between evil and target to check if evil is moving
-        float dx = transform.position.x - target.x;
-        float dz = transform.position.z - target.z;
-        if (Mathf.Sqrt(dx*dx + dz*dz) < 0.5f)
-        {
-            moving = false;
-        }
-        else
-        {
-            moving = true;
         }
     }
     void VisualizeEvil() {
@@ -79,5 +57,12 @@ public class EvilTracing : MonoBehaviour
             particleSystem.Stop();
             // Debug.Log("stop");
         }
+    }
+    float CalcPathDistance(NavMeshPath path) {
+        float route_length = 0.0f;
+        for (int i = 0; i < path.corners.Length - 1; i++) {
+            route_length += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+        }
+        return route_length;
     }
 }
