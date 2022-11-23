c6d21dc62b9629666a022453b9a71f00fb537120
ChenWish <b08901181@ntu.edu.tw>
2022-11-23 12:52:43 +0800

    delete comment

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -36,7 +36,6 @@ public class Grenade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(transform.position);
         timer += Time.deltaTime;
         if (timer >= flyingTime)
         {
