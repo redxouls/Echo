f9817c3b9148236dddd4e1c43727d8c9f27a9604
ChenWish <b08901181@ntu.edu.tw>
2022-11-27 11:38:48 +0800

    add physic material for bouncing grenade

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -6,19 +6,20 @@ public class Grenade : MonoBehaviour
 {
     
     public SoundWaveManager soundWaveManager;
-    public float explosionTime; // how much time will take in the whole explosion (lighting up the world)
-    public float flyingTime; // how long can grenade fly without collide with object
+    public float lifeSpan;
+    // public float flyingTime; // how long can grenade fly without collide with object
     public int numberOfWave; // how many wave will be created after explosion
-    public float explosionInterval; // interval between each explosion wave
+    // public float explosionInterval; // interval between each explosion wave
+    public int countOfWave;
 
     // grenade wave parameters
-    public float thickness; 
-    public float lifeSpan;
-    public float speed;
+    public float waveThickness; 
+    public float waveLifeSpan;
+    public float waveSpeed;
 
-    private float timer;
-    private bool exploding;
-    private Vector3 targetPos;
+    // private float timer;
+    // private bool exploding;
+    // private Vector3 targetPos;
 
     // audio
     public AudioClip clip;
@@ -27,47 +28,52 @@ public class Grenade : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timer = 0f;
-        exploding = false;
-        targetPos = Vector3.zero;
+        // timer = 0f;
+        Destroy(gameObject, lifeSpan);
+        // exploding = false;
+        countOfWave = 0;
         audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= flyingTime)
-        {
-            Explode();
-        }
-        if (targetPos != Vector3.zero)
-        {
-            transform.position = targetPos;
-        }
+        // timer += Time.deltaTime;
+        // if (timer >= flyingTime)
+        // {
+        //     Explode();
+        // }
+        // if (targetPos != Vector3.zero)
+        // {
+        //     transform.position = targetPos;
+        // }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        Explode();
+        if (countOfWave < numberOfWave)
+        {
+            countOfWave++;
+            CreateWave();
+        }
     }
 
-    void Explode()
-    {
-        if (exploding)
-        {
-            return;
-        }
-        exploding = true;
-        targetPos = gameObject.transform.position;
-        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        gameObject.GetComponent<Rigidbody>().useGravity = false;
-        Destroy(gameObject, explosionTime);
-        for (int i = 0; i < numberOfWave; ++i)
-        {
-            Invoke("CreateWave", i * explosionInterval);
-        }
-    }
+    // void Explode()
+    // {
+    //     if (exploding)
+    //     {
+    //         return;
+    //     }
+    //     exploding = true;
+    //     // targetPos = gameObject.transform.position;
+    //     // gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    //     // gameObject.GetComponent<Rigidbody>().useGravity = false;
+    //     Destroy(gameObject, explosionTime);
+    //     // for (int i = 0; i < numberOfWave; ++i)
+    //     // {
+    //     //     Invoke("CreateWave", i * explosionInterval);
+    //     // }
+    // }
 
     void CreateWave()
     {
